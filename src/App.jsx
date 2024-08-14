39740753bf457822452bd812ed652f75de319782
azat <azatsafaryan@mail.ru>
2024-08-14 12:26:40 +0500

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles/App.css';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/UI/navbar/Navbar';
 import AppRouter from './components/AppRouter';
+import { AuthContext } from './context';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
-    <BrowserRouter>
+    <AuthContext.Provider value={{
+      isAuth,
+      setIsAuth
+    }}>
+      <BrowserRouter>
       <Navbar />
-      <AppRouter />
-     
+      <AppRouter />   
     </BrowserRouter>
+
+    </AuthContext.Provider>
+    
   );
 }
 
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import About from '../pages/About';
-import Posts from '../pages/Posts';
-import Error from '../pages/Error';
-import PostIdPages from '../pages/PostIdPages';
 import { publicRoutes, privateRoutes } from './router';
+import { AuthContext } from '../context';
 
 const AppRouter = () => {
+  const {isAuth = useContext(AuthContext);
+  console.log(isAuth);
   return (
-    
-      <Routes>
-
+    isAuth 
+    ?
+    <Routes>
+        isAuth 
+        ? 
         {privateRoutes.map(route =>
-        <Route
-          key={route.path}
-          element={<route.element/>}
-          path={route.path}
-          />
-          )}
+                <Route
+                  key={route.path}
+                  element={<route.element/>}
+                  path={route.path}
+                  />
+                  )}
+                <Route path="*" element={<Navigate to="/posts" replace />} /> 
+      </Routes>
+    :
+    <Routes>
         {publicRoutes.map(route =>
         <Route
           key={route.path}
@@ -25,8 +30,7 @@ const AppRouter = () => {
           path={route.path}
           />
           )}
-        <Route path="*" element={<Navigate to="/posts" replace />} /> 
-      
+        <Route path="*" element={<Navigate to="/login" replace />} /> 
       </Routes>
    
   );
